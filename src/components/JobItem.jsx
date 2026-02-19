30e50b2e4829117598ac084fe71509e8525d738f
vickyraineriUADE <vraineri@uade.edu.ar>
2026-02-19 18:01:58 -0300

    add applicationId to job application request

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -21,6 +21,7 @@ function JobItem({ job, candidate }) {
         uuid: candidate.uuid,
         jobId: job.id,
         candidateId: candidate.candidateId,
+        applicationId: candidate.applicationId,
         repoUrl,
       });
 
